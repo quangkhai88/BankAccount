61f8e6c111f75860ea4f8fc48238a0fe382d684f
quangkhai88 <canhcung88@gmail.com>
2022-02-12 00:23:37 +0100

    trading room module

--- src/main/java/tradingRoom/Book.java
+++ src/main/java/tradingRoom/Book.java
@@ -10,25 +10,14 @@ import java.util.List;
 
 public final class Book  implements TradingCollection{
 
-    private final List<Wallet> wallets = new ArrayList<>();
-    private final List<Book> books = new ArrayList<>();
+    private final List<TradingCollection> tradingCollections = new ArrayList<>();
 
     @Override
     public BigDecimal getValue() {
-        BigDecimal walletValue = getCollectionValue(this.wallets);
-        BigDecimal booksValue = getCollectionValue(this.books);
-        return booksValue.add(walletValue);
+        return tradingCollections.stream().map(TradingCollection::getValue).reduce(new BigDecimal(0), BigDecimal::add);
     }
 
-    private BigDecimal getCollectionValue(List<? extends TradingCollection> list) {
-        return list.stream().map(TradingCollection::getValue).reduce(new BigDecimal(0), BigDecimal::add);
-    }
-
-    public void addBook(Book book){
-        this.books.add(book);
-    }
-
-    public void addWallet(Wallet wallet) {
-        this.wallets.add(wallet);
+    public void addCollection(TradingCollection tradingCollection) {
+        this.tradingCollections.add(tradingCollection);
     }
 }
